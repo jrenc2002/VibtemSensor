e468fa82df39d4ba4779dcf878b293db3a7e1e39
Jrenc <87536202+lulu546@users.noreply.github.com>
2023-08-11 01:45:21 +0800

    1.增加了分页重命名的逻辑。

--- src/components/NavigationView.vue
+++ src/components/NavigationView.vue
@@ -29,15 +29,28 @@
             </div>
           </div>
           <ul class="-mx-2 mt-4 space-y-1" role="list">
-            <li v-for="team in teams" :key="team.name">
-              <a :class="[team.current ? 'bg-white rounded-md shadow text-indigo-600' : 'text-gray-700 hover:text-gray-600 hover:bg-white hover:shadow', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']"
-                 :href="team.href" @click.stop="changebindPage(team.id,$event)">
+            <li v-for="team in pages" :key="team.name">
+              <div
+                  :class="[team.current ? 'bg-white rounded-md shadow text-indigo-600' : 'text-gray-700 hover:text-gray-600 hover:bg-white hover:shadow', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']"
+                  :href="team.href" @click.prevent="changebindPage(team.id,$event)"
+                  @dblclick.stop="changePageName(team.id,$event)">
                 <span
                     :class="[team.current ? 'text-indigo-600 border-indigo-600' : 'text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600', 'flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white']">{{
                     team.id
-                  }}</span>
-                <span class="truncate">{{ team.name }}</span>
-              </a>
+                  }}
+                </span>
+
+                <span v-if="team.id !== editID" class="truncate">{{ team.name }}</span>
+                <span v-else>
+                <input v-model="updateName"
+                       :placeholder="team.name"
+                       class="broder border-gray-200 text-black w-full"
+                       @keyup.enter="enterEdit(team.id,$event)"
+
+                >
+
+              </span>
+              </div>
             </li>
           </ul>
         </li>
@@ -55,7 +68,8 @@ import {useAppGlobal} from '@/store/appglobal'
 // 可以在组件中的任意位置访问 `store` 变量 ✨
 const AppGlobal = useAppGlobal()
 const chancepage = ref(AppGlobal.pageChance)
-
+const editID = ref(-1)
+const updateName = ref('')
 const route = useRoute()
 const navigation = computed(() => [
   {name: '显示界面', href: '/mainview', icon: TvIcon, current: route.path === '/mainview'},
@@ -63,19 +77,38 @@ const navigation = computed(() => [
   {name: '设置界面', href: '/setiew', icon: Cog8ToothIcon, current: route.path === '/setiew'},
   {name: '报警数据', href: '/alarmview', icon: ServerStackIcon, count: '20+', current: route.path === '/alarmview'}
 ])
-const teams = computed(() => [
-  {id: 1, name: '一号工厂', href: '#', current: chancepage.value == 1},
-  {id: 2, name: '二号工厂', href: '#', current: chancepage.value == 2},
-  {id: 3, name: '三号工厂', href: '#', current: chancepage.value == 3},
-  {id: 4, name: '四号工厂', href: '#', current: chancepage.value == 4},
-  {id: 5, name: '五号工厂', href: '#', current: chancepage.value == 5},
 
+const basePages = ref([
+  {id: 1, name: '一号工厂', href: '#'},
+  {id: 2, name: '二号工厂', href: '#'},
+  {id: 3, name: '三号工厂', href: '#'},
+  {id: 4, name: '四号工厂', href: '#'},
+  {id: 5, name: '五号工厂', href: '#'},
 ])
+
+const pages = computed(() => {
+  return basePages.value.map(page => ({
+    ...page,
+    current: page.id === chancepage.value
+  }))
+})
+
 // 切换绑定的分页
-const changebindPage = (e: number) => {
-  chancepage.value = e;
-  AppGlobal.updatePageChance(e);
+const changebindPage = (id: number) => {
+  chancepage.value = id;
+  AppGlobal.updatePageChance(id);
 
 }
+// 改变分页名称
+const changePageName = (id: number) => {
+  editID.value = id
+
+}
+// 监听分页名称输入框
+const enterEdit = (id: number) => {
+  basePages.value[id - 1].name = updateName.value;
+  editID.value = -1;
+}
+
 
 </script>
